7f4a31c3c8f049c36316a34f51a211236cc8ec64
coopflow <38353969+coopflow@users.noreply.github.com>
2020-09-16 19:21:10 +0200

    Remove comments

--- plugin.d.ts
+++ plugin.d.ts
@@ -6,26 +6,19 @@ interface FastifyStripeOptions extends Omit<Stripe.StripeConfig, "appInfo"> {
   name?: string;
 }
 
-// Optionally, you can add any additional exports.
 export interface FastifyStripeNamedInstance {
   (name: string): Stripe;
 }
 
-// Here we are exporting the decorator we added.
 export interface FastifyStripe {
   stripe: Stripe | FastifyStripeNamedInstance;
 }
 
-// Most importantly, use declaration merging to add the custom property to the Fastify type system
 declare module 'fastify' {
   interface FastifyInstance {
     stripe: FastifyStripe;
   }
 }
 
-// fastify-plugin automatically adds named export, so be sure to add also this type
-// the variable name is derived from `options.name` property if `module.exports.myPlugin` is missing
 export const FastifyStripePlugin: FastifyPluginCallback<FastifyStripeOptions>
-
-// fastify-plugin automatically adds `.default` property to the exported plugin. See the note below
 export default FastifyStripePlugin
